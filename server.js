6c5dec3023c110a733aedc3841f38c35238d4436
ivandw <ivandw@gmail.com>
2019-08-17 05:39:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-	  const browser = await puppeteer.launch();
-	  const page = await browser.newPage();
-	  await page.goto('https://example.com');
-	  await page.screenshot({path: 'example.png'});
+		  const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox', '--enable-logging', '--v=1']});
+		  const page = await browser.newPage();
+		  await page.goto('https://www.google.com.ar');
+		  await page.screenshot({path: 'example.png'});
 
-	  await browser.close();
+		  await browser.close();
 })();
